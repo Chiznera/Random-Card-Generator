cc55c2ce57c886b315060e45d174c563c8b00b4f
Chiznera <chiznera@gmail.com>
2022-06-02 00:59:43 +0000

    Structure in place, need to get JS into HTML

--- src/app.js
+++ src/app.js
@@ -2,10 +2,35 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let Suit = [`Spade`, `Club`, `Heart`, `Diamond`];
+  let Value = [
+    `Ace`,
+    `2`,
+    `3`,
+    `4`,
+    `5`,
+    `6`,
+    `7`,
+    `8`,
+    `9`,
+    `Jack`,
+    `Queen`,
+    `King`,
+  ];
+  const cards = [];
+  for (let s = 0; s < Suit.length; s++) {
+    for (let v = 0; v < Value.length; v++) {
+      cards.push([Suit[s], Value[v]]);
+    }
+  }
+
+  function cardPull() {
+    return cards[Math.floor(Math.random() * cards.length)];
+  }
+  
+  function randomCard() {
+    let cardSuit = document.querySelectorAll(`.cardSuit`)
+    cardSuit.innerhtml = (`${cardPull[0]}`)
+  }
 };
